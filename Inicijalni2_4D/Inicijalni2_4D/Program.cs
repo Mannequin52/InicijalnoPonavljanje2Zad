30a859779fbc8cd0ae8092b6df4c3d8b8224b8d6
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-09-11 10:40:52 +0200

    Add files via upload

---
+++ Inicijalni2_4D/Inicijalni2_4D/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni2_4D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Napišite program u kojem ćete zatražiti od korisnika
+             * da unese niz znakova te će svaki razmak zamijeniti znakom '_'.*/
+            
+            Console.WriteLine("Unesi rečenicu: ");
+            string a = Console.ReadLine();
+            string b = a.Replace(" ", "_");
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
